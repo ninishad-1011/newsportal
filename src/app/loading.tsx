58966de487e8057a61cf3bf5ac6934f9df398132
ninishad-1011 <nafiul1011@gmail.com>
2025-12-31 03:01:46 +0600

    remove comment

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -6,10 +6,9 @@ export default function Loading() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 dark:bg-gray-900 text-center">
       
-      {/* Spinner */}
+      
       <FiLoader className="animate-spin text-5xl text-green-500 mb-4" />
 
-      {/* Text */}
       <h2 className="text-xl font-semibold text-gray-800 dark:text-gray-200">
         লোড হচ্ছে...
       </h2>
